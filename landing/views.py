bc3dd9f4e8ec388fa17fc2ff4a98b15b651e4fe5
henrygbc <henrygbc@henrygbc.(none)>
2014-11-23 15:29:46 -0430

    Prototipo listo

--- landing/views.py
+++ landing/views.py
@@ -1,10 +1,11 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-from django.views.generic import DetailView, FormView, ListView
+from django.views.generic import DetailView, FormView, ListView, DeleteView
 from landing.forms import LoginForm
 from landing.models import Campos, Post
 
@@ -14,6 +15,31 @@ def home(request):
     template = 'index.html'
     return render(request, template, locals())
 
+def inicio(request):
+    template = 'inicio.html'
+    if request.method == 'POST':
+        usuario = request.POST['username']
+        clave = request.POST['password']
+        log = False
+        user = authenticate(username=usuario, password=clave)
+        if user is not None:
+            # the password verified for the user
+            if user.is_active:
+                print("User is valid, active and authenticated")
+                login(request, user)
+                return HttpResponseRedirect('/inicio/')
+            else:
+                print("The password is valid, but the account has been disabled!")
+        else:
+                # the authentication system was unable to verify the username and password
+            print("The username and password were incorrect.")
+            log = True
+            return  render(request, template, locals())
+    return render(request, template, locals())
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/inicio/')
 
 def crear(request):
     template = 'crear.html'
@@ -61,3 +87,29 @@ class PostsView(ListView):
 class PostDetailView(DetailView):
     model = Post
     template_name = 'post.html'
+
+
+def delete(request, id):
+    post = get_object_or_404(Post, pk=id)
+    post.delete()
+    return HttpResponseRedirect('/posts/')
+
+
+def editar(request, id):
+    template ="edit.html"
+    post = get_object_or_404(Post, pk=id)
+    if request.method == 'POST':
+        post.titulo=request.POST['titulo']
+        post.resumen=request.POST['resumen']
+        post.contenido=request.POST['parrafo']
+
+        if request.FILES.get('imagen', False ):
+            post.imagen = request.FILES.get('imagen', False)
+        if request.FILES.get('archivo', False ):
+            post.archivo = request.FILES.get('archivo', False)
+
+
+        post.save()
+        return HttpResponseRedirect('/posts/')
+
+    return render(request, template, locals())
--- landing_company/urls.py
+++ landing_company/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-from landing.views import home, crear, PostsView, PostDetailView
+from landing.views import home, crear, PostsView, PostDetailView, delete, editar, inicio, logout_view
 
 
 urlpatterns = patterns('',
@@ -10,9 +10,13 @@ urlpatterns = patterns('',
     # url(r'^$', 'landing_company.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', home, name="home"),
+    url(r'^inicio/', inicio, name="inicio"),
     url(r'^crear/', crear, name="crear"),
     url(r'^posts/', PostsView.as_view(), name='posts'),
     url(r'^post/(?P<slug>[-_\w]+)/$', PostDetailView.as_view(), name='post'),
+    url(r'^editar/(\d+)$', editar, name='editar'),
+    url(r'^eliminar/(\d+)$', delete, name='delete'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^summernote/', include('django_summernote.urls')),
+    url(r'^logout/', logout_view, name='logout'),
 )+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
